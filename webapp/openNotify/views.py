7e0536a99271da93329de4063b89599f398413b3
Ubuntu <ubuntu@ip-172-31-36-49.ap-northeast-1.compute.internal>
2019-06-12 14:42:38 +0000

    暫定的にrequest.responseのタイムスタンプを追加

--- webapp/openNotify/views.py
+++ webapp/openNotify/views.py
@@ -8,7 +8,9 @@ import requests
 
 
 def ai_analysis_log(request):
-    headers = {"content-type": "application/json"}
+    # リクエストのタイムスタンプは暫定的に、ここで現在時刻を取得している
+    request_timestamp = datetime.now()
+    headers = {"content-type": "application/json", "Date": req_time }
     IMAGE_PATH = "image/d03f1d36ca69348c51aa/c413eac329e1c0d03/test.jpg"
     URLofAPI = "http://example.com/"
 
@@ -16,6 +18,9 @@ def ai_analysis_log(request):
 
     # POSTパラメータを、data引数で指定
     response = requests.post(URLofAPI, data=parameters, headers=headers)
+
+    # レスポンスのタイムスタンプ
+    response_timestamp = response.headers['date']
 
     # data は 辞書型。　(loads()はJSON文字列を辞書型に変換)
     data = json.loads(response.text)
@@ -26,9 +31,6 @@ def ai_analysis_log(request):
     classOfImage = data["estimated_data"]["class"]
     confidence = data["estimated_data"]["confidence"]
 
-    # 下記は配信されないので、アクセスログから取得する
-    request_timestamp = data[""][""]
-    response_timestamp = data[""][""]
 
     item = Ai_analysis_log(
         image_path=image_path,
