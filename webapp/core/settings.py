c7a5ca8742fe2724b419832731a781ef10e8a58e
Ubuntu <ubuntu@ip-172-31-36-49.ap-northeast-1.compute.internal>
2019-06-12 09:49:31 +0000

    secretkeyを非公開に

--- webapp/core/settings.py
+++ webapp/core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '&-nok@jac(i$od*4*qz#)43t)!4d)r_g&#hz29(kvl&8px#js_'
+
 SECRET_KEY = local_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
